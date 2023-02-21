b988f253d4e2dfc1f3475bc4ca1997f824719aea
Ivan Martin Aviles Moreira <ivanaviles569@gmail.com>
2023-02-21 00:37:21 -0700

    noveno dia de servicio en casa

---
+++ plantilla/components/footer.php
@@ -0,0 +1,92 @@
+<!-- Footer Start -->
+<footer id="rs-footer" class="rs-footer footer-main-home footer-style">
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 pr-20 md-pr-15 md-mb-20">
+							<div class="footer-logo mb-51 md-mb-30">
+								<a href="index.html"><img src="assets/images/logo-white.png" alt=""></a>
+							</div>
+							<div class="textwidget">
+								<p class="pb-20">En ASIEC® nos apasiona proveer productos y servicios en seguridad industrial, prevención de riesgos y cuidado del medio ambiente. 
+									Fomentamos una cultura en prevención de riesgos y de mejora continua en los procesos, para proteger al capital humano y los equipos en la industria, generando valor para nuestros clientes, nuestros asociados y la sociedad.</p>
+								<p class="pb-25"><strong>Opening Hours</strong> : Monday – Sat: 9am to 6pm</p>
+							</div>
+							<ul class="footer-social md-mb-30">  
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>                     
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>                     
+                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>                     
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>                     
+                          	</ul>
+						</div>
+						<div class="col-lg-3 md-mb-10">
+							<h3 class="footer-title">Blogs recientes</h3>
+							<ul class="address-widget">
+                                <li>
+                                    <i class="fi fi-rr-map-marker-home"></i>
+                                    <div class="desc">
+                                    	Ta-134/A, Gulshan Badda Link<br>
+                                    	Rd, Dhaka
+									</div>
+                                </li>
+                                <li>
+                                    <i class="fi fi-rr-phone-call"></i>
+                                    <div class="desc">
+                                       <a href="tel:(+880)15569569365">(+880)155 69569 365</a><br>
+                                       <a href="tel:(+880)15569569366">(+880)155 69569 366</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="fi fi-rr-envelope-plus"></i>
+                                    <div class="desc">
+                                        <a href="mailto:support@devsdesign.com">support@devsdesign.com</a><br>
+                                        <a href="mailto:infoname@gmail.com">infoname@gmail.com</a>
+                                    </div>
+                                </li>
+                            </ul>
+						</div>
+						<div class="col-lg-3 md-mb-10">
+							<h3 class="footer-title">Enlaces utiles</h3>
+							<ul class="site-map">
+                                <li><a href="general-construction.html">Acerca de Nosotros</a></li>
+                                <li><a href="property-maintenance.html">FAQ</a></li>
+                                <li><a href="project-managment.html">Servicios</a></li>
+                                <li><a href="virtual-design-build.html">Clientes</a></li>
+                                <li><a href="preconstruction.html">Proyectos</a></li>
+                                <li><a href="design-build.html">Aviso de privacidad</a></li>
+                            </ul>
+						</div>
+						<div class="col-lg-3">
+							<h3 class="footer-title">Newsletter</h3>
+							<p class="widget-desc">Tetur adipiscing elit, sed do eiusmod tempor in cididunt ut labore.</p>
+							<p>
+                                <input type="email" name="email" placeholder="Your Email Address" required="">
+                                <input type="submit" value="Subscribe Now">
+                            </p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-bottom">
+                <div class="container">                    
+                    <div class="row y-middle">
+                        <div class="col-lg-6 md-mb-10 text-lg-end text-center order-last">
+                            <ul class="copy-right-menu">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="https://devsdesign.net/products/html/konstruk/services.html">Services</a></li>
+                                <li><a href="#">Pages</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="copyright text-lg-start text-center">
+                                <p>© 2022 Konstruk.  Designed By <a href="https://devsdesign.net" target="blank">Devsdesign</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		</footer>
+		<!-- Footer End -->
---
+++ plantilla/index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="zxx"> 
+	<head>
+		<?php include('components/head.php') ?>
+		
+	</head>
+	<body class="default-home">
+		
+		<div class="offwrap"></div>
+
+		<!--Preloader start here-->
+	   	<div id="pre-load">
+            <div id="loader" class="loader">
+                <div class="loader-container">
+                    <div class="loader-icon"><img src="assets/images/fav.png" alt="Konstruk - Construction & Building Html Template "></div>
+                </div>
+            </div>              
+        </div>
+		<!--Preloader area end here-->
+			
+		<!-- Main content Start -->
+		<div class="main-content">
+
+			<!--Full width header Start-->
+			<div class="full-width-header">
+				<!--Header Start-->
+				<?php include('components/header.php')?>
+				<!--Header End-->
+			</div>
+			<!--Full width header End-->
+
+			<!-- Slider Start -->
+			<div id="rs-slider" class="rs-slider rs-slider-style2">
+			    <div class="bend niceties">
+			        <div id="nivoSlider" class="slides">
+			            <img src="assets/images/slider/style1/h1-1.jpg" alt="" title="#slide-1" />
+			            <img class="slies-img" src="assets/images/slider/style1/h1-2.jpg" alt="" title="#slide-22" />
+			        </div>
+			        <!-- Slide 1 -->
+			        <div id="slide-1" class="slider-direction">
+			           <div class="container">
+			           		<div class="content-part">
+			           		    <span class="sl-subtitle">
+			           		    	<img class="sl-icons" src="assets/images/slider/style1/shapes/icons3.png" alt="Images">
+			           		   		Construction Business
+			           			</span>
+			           		    <h1 class="sl-title">
+			           		    	We build something<br>
+			           		    	new and consistent.<br>
+			           		    </h1>
+			           		    <p class="sl-desc">
+			           		    	No matter where or what you want to build, we mobilize the right experts<br> 
+			           		    	to drive value and realize your project consistent goals. 		
+			           		    </p>
+			           		   	<div class="slider-bottom">
+			           		   	    <a class="readon more" href="contact.html">Discover More</a>
+			           		   	</div>
+			           		</div>
+			           		<div class="animation-wrap">
+			           			<div class="animate-style one">
+			           				<img class="sl-icons1" src="assets/images/slider/style1/shapes/icons1.png" alt="Images">
+			           			</div>
+			           			<div class="animate-style two">
+			           				<img class="sl-icons1" src="assets/images/slider/style1/shapes/icons2.png" alt="Images">
+			           			</div>
+			           			<div class="animate-style three">
+			           				<img class="sl-icons1" src="assets/images/slider/style1/shapes/icons2.png" alt="Images">
+			           			</div>
+			           		</div>
+			           </div>
+			        </div>
+			        <!-- Slide 2 -->
+			        <div id="slide-22" class="slider-direction">
+			        	<div class="container-side-slide2">
+		        			<div class="content-part">
+		        			    <span class="sl-subtitle1">
+		        			    	<img class="sl-icons" src="assets/images/slider/style1/shapes/icons3.png" alt="Images">
+		        			    	Construction Business
+		        			    </span>
+		        			    <h1 class="sl-title1">
+		        			    	We build something<br>
+		        			    	new and consistent.<br>
+		        			    </h1>
+		        			    <p class="sl-desc1">
+		        			    	No matter where or what you want to build, we mobilize the right experts<br> 
+		        			    	to drive value and realize your project consistent goals.
+		        			    </p>
+		        			   	<div class="slider-bottom">
+		        			   	    <a class="readon more" href="contact.html">Discover More</a>
+		        			   	</div>
+		        			</div>
+		        			<div class="animation-wrap2">
+		        				<div class="animate-style one">
+		        					<img class="sl-icons2" src="assets/images/slider/style1/shapes/icons1.png" alt="Images">
+		        				</div>
+		        				<div class="animate-style two">
+		        					<img class="sl-icons2" src="assets/images/slider/style1/shapes/icons2.png" alt="Images">
+		        				</div>
+		        				<div class="animate-style three">
+		        					<img class="sl-icons2" src="assets/images/slider/style1/shapes/icons2.png" alt="Images">
+		        				</div>
+		        			</div>
+			        	</div>
+			        </div>
+			    </div>
+			</div>
+			<!-- Slider End -->
+
+			<!-- Services Section Start -->
+			<div id="rs-services" class="rs-services services-style3 bg7 pt-120 pb-120 md-pt-70 md-pb-80">
+			    <div class="container">
+					<div class="row justificado">
+						<div class="sec-title2 text-center mb-65 md-mb-45">
+							<h2 class="title pb-25">¿POR QUÉ ESCOGERNOS?</h2>
+							<span class="title-color">Somos una empresa mexicana con especialistas en asesoría enfocada a la seguridad industrial, salud ocupacional, <br>
+								emergencias, protección al medio ambiente y capacitaciones para todo tipo y tamaño de empresas.</span>
+							
+							<div class="heading-border-line"></div>
+						</div>
+			    		<div class="col-lg-4 col-md-6 mb-20">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon1.png" alt="Services">
+			    				</div>
+			    				<div class="services-text ">
+			    					<h4 class="title">Honestidad</h4>
+			    					<p class="services-txt "> Buscamos en todo momento el bien común, con congruencia a nuestros principios. </p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    		<div class="col-lg-4 col-md-6 mb-20">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon2.png" alt="Services">
+			    				</div>
+			    				<div class="services-text">
+			    					<h4 class="title">Compromiso</h4>
+			    					<p class="services-txt ">Cumplimos en todo momento con los acuerdos realizados, actuando de forma oportuna y proactiva</p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    		<div class="col-lg-4 col-md-6 mb-20">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon3.png" alt="Services">
+			    				</div>
+			    				<div class="services-text">
+			    					<h4 class="title">Credebilidad</h4>
+			    					<p class="services-txt ">Servimos de forma sincera y con transparencia, satisfaciendo las necesidades de nuestros clientes de manra integral</p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    		<div class="col-lg-4 col-md-6 md-mb-20">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon4.png" alt="Services">
+			    				</div>
+			    				<div class="services-text">
+			    					<h4 class="title">Empatía</h4>
+			    					<p class="services-txt ">Percibimos y resolvemos la necesidad de nuestros clientes y la sociedfad, aportando valor con cada una de nuestras soluciones.</p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    		<div class="col-lg-4 col-md-6 sm-mb-20">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon5.png" alt="Services">
+			    				</div>
+			    				<div class="services-text">
+			    					<h4 class="title">Responsibilidad</h4>
+			    					<p class="services-txt ">Somos responsables de nuestros actos, ante la sociedad y el medio ambiente. </p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    		<div class="col-lg-4 col-md-6">
+			    			<div class="services-item">
+			    				<div class="services-icon">
+			    					<img src="assets/images/services/style3/ser-icon6.png" alt="Services">
+			    				</div>
+			    				<div class="services-text">
+			    					<h4 class="title">Innovacion</h4>
+			    					<p class="services-txt ">Nos comprometimos con el desarrollo de nuestros clientes, permaneciendo a la vanguardia, superando cualquier obstaculo con soluciones efectivas y prácticas</p>
+			    				</div>
+			    			</div>
+			    		</div>
+			    	</div>
+			    </div>
+			</div>
+			<!-- Services Section End -->
+
+			<!-- Testimonial Section Start -->
+			<div id="rs-blog" class="rs-blog blog-main-home blog-modify2 bg5 pt-120 pb-115 md-pt-0 md-pb-80">
+			    <div class="container">  
+			        <div class="sec-title2 text-center mb-65 md-mb-45">
+			        	<h2 class="title pb-25">
+			        		Blogs recientes
+			        	</h2>
+			        </div>
+			        <div class="rs-carousel owl-carousel" data-loop="true" data-items="3" data-margin="30" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="2" data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="2" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="3" data-md-device-nav="false" data-md-device-dots="false">
+			            <div class="blog-item">
+			                <div class="image-wrap">
+			                    <a href="blog-single.html"><img src="assets/images/blog/style1/1.jpg" alt=""></a>
+			                </div>
+			                <div class="blog-content">
+			                	<ul class="blog-meta">
+			                		<li class="date"><i class="fa fa-calendar-check-o"></i>7 febrero,2023</li>
+	                           		<li class="admin"><i class="fa fa-user-o"></i>Asiec</li>
+	                           	</ul>
+			                    <h3 class="blog-title"><a href="blog-single.html">Desinfecta correctamente: Una guia para limpiar y proteger tu espacio laboral</a></h3>
+			                    <p class="desc">A pesar de que no estamos en la época con el mayor auge de Covid19, siempre es importante respetar las reglas básicas de higiene, no solo[…]</p>
+			                    <div class="blog-button blog-bnt2">
+			                    	<a href="blog-single.html">
+                              			<span class="btn-text">
+                              				Ver más
+                              			</span>
+                              			<i class="fi fi-rr-arrow-small-right"></i>
+                              		</a> 
+			                    </div>
+			                </div>
+			            </div>
+			            <div class="blog-item">
+			                <div class="image-wrap">
+			                    <a href="blog-single.html"><img src="assets/images/blog/style1/2.jpg" alt=""></a>
+			                </div>
+			                <div class="blog-content">
+			                	<ul class="blog-meta">
+			                		<li class="date"><i class="fa fa-calendar-check-o"></i>27 Enero, 2023</li>
+	                           		<li class="admin"><i class="fa fa-user-o"></i>Asiec</li>
+	                           	</ul>
+			                    <h3 class="blog-title"><a href="blog-single.html">Aprende a actuar ante un accidente de trabajo</a></h3>
+			                    <p class="desc">Imagina que es un día normal de trabajo, no importa la actividad que estés desempeñando o como lo hagas, no es más que un día común de[…]</p>
+			                    <div class="blog-button blog-bnt2">
+			                    	<a href="blog-single.html">
+                              			<span class="btn-text">
+                              				Ver más                           
+                              			</span>
+                              			<i class="fi fi-rr-arrow-small-right"></i>
+                              		</a> 
+			                    </div>
+			                </div>
+			            </div>
+			            <div class="blog-item">
+			                <div class="image-wrap">
+			                    <a href="blog-single.html"><img src="assets/images/blog/style1/3.jpg" alt=""></a>
+			                </div>
+			                <div class="blog-content">
+			                	<ul class="blog-meta">
+			                		<li class="date"><i class="fa fa-calendar-check-o"></i>11 Enero, 2023</li>
+	                           		<li class="admin"><i class="fa fa-user-o"></i>Asiec</li>
+	                           	</ul>
+			                    <h3 class="blog-title"><a href="blog-single.html">¿Qué dice ka NOM sobre las herramientas para las actividades agrícolas?</a></h3>
+			                    <p class="desc">Como recordarás, hace no mucho hablamos sobre la NOM y como clasifica las señales de seguridad en las empresas. Para recapitular un […]</p>
+			                    <div class="blog-button blog-bnt2">
+			                    	<a href="blog-single.html">
+                              			<span class="btn-text">
+                              				Ver más                           
+                              			</span>
+                              			<i class="fi fi-rr-arrow-small-right"></i>
+                              		</a> 
+			                    </div>
+			                </div>
+			            </div>
+			            <div class="blog-item">
+			                <div class="image-wrap">
+			                    <a href="blog-single.html"><img src="assets/images/blog/style1/4.jpg" alt=""></a>
+			                </div>
+			                <div class="blog-content">
+			                	<ul class="blog-meta">
+			                		<li class="date"><i class="fa fa-calendar-check-o"></i>26 April 2022</li>
+	                           		<li class="admin"><i class="fa fa-user-o"></i>Admin</li>
+	                           	</ul>
+			                    <h3 class="blog-title"><a href="blog-single.html">Ejemplo Construction executive holds a very special place</a></h3>
+			                    <p class="desc">The brand has responded by partnering with skincare experts on ejemplo...</p>
+			                    <div class="blog-button blog-bnt2">
+			                    	<a href="blog-single.html">
+                              			<span class="btn-text">
+                              				Read More                           
+                              			</span>
+                              			<i class="fi fi-rr-arrow-small-right"></i>
+                              		</a> 
+			                    </div>
+			                </div>
+			            </div>
+			            <div class="blog-item">
+			                <div class="image-wrap">
+			                    <a href="blog-single.html"><img src="assets/images/blog/style1/5.jpg" alt=""></a>
+			                </div>
+			                <div class="blog-content">
+			                	<ul class="blog-meta">
+			                		<li class="date"><i class="fa fa-calendar-check-o"></i>26 April 2022</li>
+	                           		<li class="admin"><i class="fa fa-user-o"></i>Admin</li>
+	                           	</ul>
+			                    <h3 class="blog-title"><a href="blog-single.html">Ejemplo This is another massive sites with a ton of information</a></h3>
+			                    <p class="desc">The brand has responded by partnering with skincare experts on...</p>
+			                    <div class="blog-button blog-bnt2">
+			                    	<a href="blog-single.html">
+                              			<span class="btn-text">
+                              				Read More                           
+                              			</span>
+                              			<i class="fi fi-rr-arrow-small-right"></i>
+                              		</a> 
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			    </div>
+			</div>
+			<!-- Testimonial Section End -->
+
+			<!-- Partner End -->
+
+		</div> 
+		<?php include('components/footer.php')?>
+
+		<!-- start scrollUp  -->
+		<div id="scrollUp" class="red-color2">
+			<i class="fa fa-angle-up"></i>
+		</div>
+		<!-- End scrollUp  -->
+
+		<!-- Search Modal Start -->
+		<div aria-hidden="true" class="modal fade search-modal" role="dialog" tabindex="-1">
+		    <div class="modal-dialog modal-dialog-centered">
+		        <div class="modal-content">
+		            <div class="search-block clearfix">
+		                <form>
+		                    <div class="form-group">
+		                        <input class="form-control" placeholder="Search Here..." type="text">
+		                    </div>
+		                </form>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+		<!-- Search Modal End -->
+
+		<?php include('components/scripts.php') ?>
+	</body>
+</html>
