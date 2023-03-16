252c47d9ddb76156ad10a1c8b345ff890c9ac1f8
ivandiana17 <ivanaviles569@gmail.com>
2023-03-16 13:07:41 -0700

    servicio dia 28

---
+++ cursoVue/hola_mundo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div> Hola mundo</div>
+<button class="btn red" @click="saludar" >boton <i class="material-icons">save</i></button>
+</template>
+
+<script>
+import M from 'materialize-css'
+export default {
+  name: 'App',
+  methods:{
+    saludar(){
+      M.toast({html: 'Hola Mundo'});
+    }
+  }
+}
+</script>
