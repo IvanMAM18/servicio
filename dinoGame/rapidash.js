efe19b0c1fe5a5fc2e81a7d6b5c0f1d60c764b41
ivanMartinAM17 <106177766+ivandiana17@users.noreply.github.com>
2023-03-24 12:23:20 -0600

    Update rapidash.js

--- dinoGame/rapidash.js
+++ dinoGame/rapidash.js
@@ -8,16 +8,24 @@ class Rapidash{
         this.gravity=2;
     }
     draw(){
+        noFill();
+        rect(this.x, this.y, this.s, this.s);
         image(this.img, this.x, this.y , this.s, this.s);
     }
 
     jump(){
-        this.vy = -35;
+        if(this.y == height - this.s){
+            this.vy = -35;
+        }
     }
 
     update(){
         this.y += this.vy;
         this.vy += this.gravity;
         this.y = constrain(this.y, 0 , height - this.s);
+    }
+
+    collision(exeggutor){
+
     }
 }
