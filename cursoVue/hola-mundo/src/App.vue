1a7fdb00c3c9489b27882009725356bc6ca06d24
ivandiana17 <ivanaviles569@gmail.com>
2023-04-14 14:02:36 -0700

    Curso Vue.js

---
+++ cursoVue/hola-mundo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hola mundo</h1>
+    <button class="btn red " @click="saludar">Boton <i class="material-icons">info</i></button>
+  </div>
+</template>
+
+<script>
+import M from 'materialize-css'
+export default {
+  name: 'App',
+  mounted(){
+    this.axios.get('https://prueba.com')
+    .then(() => {
+
+    });
+  },
+  methods:{
+    saludar()
+    {
+      M.toast({html: 'Hola Mundo'});
+    }
+  }
+}
+</script>
